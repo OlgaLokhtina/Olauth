3ad01c3b17f36ee479905244d6730f94504fba8c
OlgaLokhtina <stella_3_@mail.ru>
2026-03-13 14:59:58 +0300

    begin_project

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from crud.user import user_router
+from crud.user import user_rout
 
 app = FastAPI()
 
-app.include_router(user_router)
+app.include_router(user_rout)
--- src/crud/user.py
+++ src/crud/user.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter
 from typing import Dict
 
 
-user_router = APIRouter(prefix="/api")
+user_rout = APIRouter(prefix="/api")
 
 
-@user_router.get("/healthcheck")
+@user_rout.get("/healthcheck")
 def healthcheck() -> Dict:
     return {"success": True}
